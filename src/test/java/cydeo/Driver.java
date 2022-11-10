3589bd36cc4dd40dfa23b15612baebd61d73402e
Zeynep Ozler <zynpzlr@gmail.com>
2022-11-10 15:08:47 -0500

    Driver class updated

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -48,7 +48,7 @@ public class Driver {
 
                     try {
                         // assign your grid server address
-                        String gridAddress = "52.90.101.17";
+                        String gridAddress = "50.19.170.11";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("firefox");
